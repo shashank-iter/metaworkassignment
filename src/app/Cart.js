8b8b247e97408be21609b0086c1da0bf1e10c79b
shashank-iter <pandeyshashank812@gmail.com>
2023-07-10 10:37:07 +0530

    minor fixes

--- src/app/Cart.js
+++ src/app/Cart.js
@@ -35,7 +35,7 @@ function calculateTotal(){
         aria-hidden="true"
       />
       <Transition.Root show={open} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+        <Dialog as="div" className="relative z-50 mt-10" onClose={setOpen}>
           <Transition.Child
             as={Fragment}
             enter="ease-in-out duration-500"
